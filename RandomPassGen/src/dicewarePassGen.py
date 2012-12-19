022f5065eb9cca358eb2dafdb6ad8753b08647bf
andyfrang <github@encyl.com>
2012-12-19 03:48:18 +0000

    Working version no output

--- RandomPassGen/src/dicewarePassGen.py
+++ RandomPassGen/src/dicewarePassGen.py
@@ -1,20 +1,41 @@
 #! /usr/bin/python
 
-import urllib2, sys, string
+import urllib2, sys, string, operator
 
 wordList = []
 numList = []
 numWords = 0
+passWd = []
 
-conn = urllib2.urlopen('http://www.random.org/integers/?num=10&min=1&max=6&col=1&base=10&format=plain&rnd=new')
+
+###    Open a connection to random.org
+conn = urllib2.urlopen('http://www.random.org/integers/?num=45&min=1&max=6&col=5&base=10&format=plain&rnd=new')
 html = conn.read()
-dicewords = open("src/beale.wordlist")
+ranNums = html.split('\n')
+i=0
+for each in ranNums:
+    ranNums[i] = each.replace("\t","")
+    i += 1
+ranNums = filter(None, ranNums)
+
+###    Load diceware word list
+dicewords = open("beale.wordlist")
 fileWordsAndNums = dicewords.readlines();
 
+###    Separate words and corresponding diceware number combinations in parallel lists
 for line in fileWordsAndNums:
-    wordAndNum = string.split(line, '\t')
+    wordAndNum = string.split(line,maxsplit=1)
     wordList.append(wordAndNum[1])
     numList.append(wordAndNum[0])
     numWords += 1
+
+for ranNum in ranNums:
+    ind = 0
     
+    while ranNum != numList[ind]:
+        ind += 1
+        
+    passWd.append(wordList[ind])
+    
+
     
